ea11548c9d039569f957ae3edd81358dc32d7927
Kalpan Jasani <kjasani@purdue.edu>
2018-05-15 12:25:10 -0400

    make the second heading change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const button1 = document.getElementById("button1");
 
 function changeText(ev)
 {
-    const heading1 = document.getElementById("theHeading");
-    heading1.innerHTML = "User directory for Kalpan";
+    const heading2 = document.getElementById("heading2");
+    heading2.innerHTML = "User directory for Kalpan";
 }
 
 button1.addEventListener("click", changeText);

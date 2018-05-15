732de7149f32258ce7c8a6b959764e2435a3323e
Kalpan Jasani <kjasani@purdue.edu>
2018-05-15 13:04:45 -0400

    allow a form to update heading

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
 
 function changeText(ev)
 {
+    //get the heading and the input element
     const heading2 = document.getElementById("heading2");
-    heading2.innerHTML = "User directory for Kalpan";
+    const input1 = document.getElementById("userText1");
+
+
+    //getting text from the input
+    let text1 = input1.value.toString();
+
+    //setting heading with the text (but kalpan for debugging)
+    heading2.innerHTML = text1;
 }
 
-button1.addEventListener("click", changeText);
+button2.addEventListener("click", changeText);

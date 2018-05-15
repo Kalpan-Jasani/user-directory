1e6b4e5b3074839481e05dd04275a16767828b2b
Kalpan Jasani <kjasani@purdue.edu>
2018-05-15 15:30:08 -0400

    change outlook to have two entries and clear out creation of strings

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ function changeText(ev)
     //prevent submission of form
     ev.preventDefault();
 
-    //get the heading and the input element
+    //get the userentry div tag
     const div1 = document.getElementById("userEntries");
-    const input1 = document.getElementById("userText1");
 
     //getting text from the input
-    let text1 = input1.value.toString();
+    let text1 = form1.userName.value;
+    let text2 = form1.ageEntry.value;
 
     //appending name to entries
-    let text2 = "<p>" + text1 + "</p1>";
-    div1.innerHTML += text2;
+    let text3 = `<p>${text1}, ${text2}</p>`;
+    div1.innerHTML += text3;
 
     //clear out the entry on submit
-    ev.target.reset();
+    form1.reset();
 }
 
 form1.addEventListener("submit", changeText);
+
+//TODO: have hint for entries
+//TODO: have focus on first field on load and  on reset
+//TODO: have age entry to allow only numbers
